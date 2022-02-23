c82767d48eb3629ba89c00a61a0c3f17340c352e
jyotiprakash1405 <98477023+jyotiprakash1405@users.noreply.github.com>
2022-02-23 22:50:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+var toggle_btn;
+var big_wrapper;
+
+function declare(){
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
+}
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation(){
+    // alert("Hello");
+
+    // clone the wrapper class
+
+    dark = !dark;
+    // console.log(dark);
+    let clone = big_wrapper.cloneNode(true);
+    
+    if(dark){
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    }
+    else{
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    clone.classList.add("copy");
+    main.appendChild(clone);
+
+    clone.addEventListener("animationend", () => {
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+
+        // Reset Variables
+
+        declare();
+        events();
+        signUpEvents();
+    })
+}
+
+function events(){
+    toggle_btn.addEventListener("click" , toggleAnimation);
+}
+
+events();
+
+
+
+// SignUp Form animation
+
+function signUpEvents(){
+    document.getElementById("button").addEventListener("click" , function(){
+        document.querySelector(".popup").style.display = "flex";
+    });
+    
+    document.querySelector(".close").addEventListener("click" , function(){
+        document.querySelector(".popup").style.display = "none";
+    });
+}
+
+signUpEvents();
+
+//End
+
+// Hamburger
+function onClickMenu(){
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("nav").classList.toggle("change");
+}
+
+    
+// onClickMenu();
+
+// End
+
+
+
+
+
